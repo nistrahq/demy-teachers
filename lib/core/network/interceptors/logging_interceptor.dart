6c47a3e5a8dc0d8fc211473ce58b6afc5971d44c
Salim Ramirez <salimramirezm@gmail.com>
2025-11-02 13:43:03 -0500

    feat(logging): enhance LoggingInterceptor to log headers in development mode

--- lib/core/network/interceptors/logging_interceptor.dart
+++ lib/core/network/interceptors/logging_interceptor.dart
@@ -1,12 +1,21 @@
+import 'package:demy_teachers/config/app_config.dart';
+import 'package:demy_teachers/config/environment/environment.dart';
 import 'package:demy_teachers/config/logger/logger_config.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class LoggingInterceptor extends Interceptor {
+  final AppConfig config;
+
+  LoggingInterceptor(this.config);
+
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
     LoggerConfig.log('[API →] ${options.method} ${options.uri}');
+    if (config.environment.isDev && options.headers.isNotEmpty) {
+      LoggerConfig.log('[HEADERS] ${options.headers}');
+    }
     if (options.data != null) LoggerConfig.log('[BODY] ${options.data}');
     handler.next(options);
   }
@@ -14,6 +23,9 @@ class LoggingInterceptor extends Interceptor {
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     LoggerConfig.log('[API ←] [${response.statusCode}] ${response.realUri}');
+    if (config.environment.isDev && response.headers.map.isNotEmpty) {
+      LoggerConfig.log('[RESPONSE HEADERS] ${response.headers.map}');
+    }
     handler.next(response);
   }
 
