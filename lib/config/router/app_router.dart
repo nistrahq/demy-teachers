ad563ac251406e85a56229cdf88d42b8f742f3f1
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-13 01:46:04 -0500

    feat(attendance): update router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,3 +1,7 @@
+import 'package:demy_teachers/core/di/injection.dart';
+import 'package:demy_teachers/features/attendance/presentation/blocs/attendance_bloc.dart';
+import 'package:demy_teachers/features/attendance/presentation/blocs/attendance_event.dart';
+import 'package:demy_teachers/features/attendance/presentation/pages/attendance_page.dart';
 import 'package:demy_teachers/features/auth/presentation/navigation/auth_routes.dart';
 import 'package:demy_teachers/features/home/presentation/pages/home_page.dart';
 import 'package:demy_teachers/features/profile/presentation/pages/profile_page.dart';
@@ -5,6 +9,7 @@ import 'package:demy_teachers/features/settings/presentation/pages/settings_page
 import 'package:demy_teachers/features/splash/presentation/navigation/splash_routes.dart';
 import 'package:demy_teachers/shared/widgets/bottom_navigation_bar_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
@@ -50,6 +55,21 @@ final GoRouter appRouter = GoRouter(
               path: '/settings',
               name: 'settings',
               builder: (context, state) => const SettingsPage(),
+              routes: [
+                GoRoute(
+                  path: 'attendance',
+                  name: 'attendance',
+                  builder: (context, state) {
+                    
+                    final bloc = getIt<AttendanceBloc>()
+                      ..add(const AttendanceInitRequested());
+                    return BlocProvider.value(
+                      value: bloc,
+                      child: const AttendancePage(),
+                    );
+                  },
+                ),
+              ],
             ),
           ],
         ),
