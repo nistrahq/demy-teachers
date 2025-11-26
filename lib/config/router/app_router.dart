193e4607c8e1efc74cdd1a3bd08eedf16b296eb5
Kyrubi <u20221c486@upc.edu.pe>
2025-11-25 19:30:05 -0500

    refactor(router): remove unused ProfileBloc imports

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,15 +1,11 @@
 import 'package:demy_teachers/features/auth/presentation/navigation/auth_routes.dart';
 import 'package:demy_teachers/features/home/presentation/pages/home_page.dart';
-import 'package:demy_teachers/features/profile/presentation/blocs/profile_bloc.dart';
-import 'package:demy_teachers/features/profile/presentation/blocs/profile_event.dart';
 import 'package:demy_teachers/features/profile/presentation/pages/profile_page.dart';
 import 'package:demy_teachers/features/settings/presentation/pages/settings_page.dart';
 import 'package:demy_teachers/features/splash/presentation/navigation/splash_routes.dart';
 import 'package:demy_teachers/shared/widgets/bottom_navigation_bar_widget.dart';
 import 'package:demy_teachers/features/schedule/presentation/navigation/schedule_routes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
