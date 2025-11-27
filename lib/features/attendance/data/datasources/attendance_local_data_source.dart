d2a109e17c37a7ee82e49405b040afed9ff1bd56
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-27 00:09:01 -0500

    feat(attendance): add local data source

--- lib/features/attendance/data/datasources/attendance_local_data_source.dart
+++
@@ -1,9 +0,0 @@
-import 'package:demy_teachers/features/attendance/domain/entities/class_attendance.dart';
-
-abstract class AttendanceLocalDataSource {
-
-  Future<void> cacheClassAttendances(List<ClassAttendance> attendances);
-
-
-  Future<List<ClassAttendance>?> getCachedClassAttendances();
-}
