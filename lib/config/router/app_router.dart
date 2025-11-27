c827b3653f6bb8ad6c4f4dcaa2f3716dd514c2b2
diegovilcasaboya <diegovilcatut@gmail.com>
2025-11-27 00:05:51 -0500

    feat(attendance): update app router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:demy_teachers/core/di/injection.dart';
 import 'package:demy_teachers/features/attendance/presentation/blocs/attendance_bloc.dart';
 import 'package:demy_teachers/features/attendance/presentation/blocs/attendance_event.dart';
-import 'package:demy_teachers/features/attendance/presentation/pages/attendance_page.dart';
+import 'package:demy_teachers/features/attendance/presentation/pages/take_attendance_page.dart'; // Asegúrate de importar esto
 import 'package:demy_teachers/features/auth/presentation/navigation/auth_routes.dart';
 import 'package:demy_teachers/features/home/presentation/pages/home_page.dart';
 import 'package:demy_teachers/features/profile/presentation/blocs/profile_bloc.dart';
@@ -22,6 +22,21 @@ final GoRouter appRouter = GoRouter(
     ...splashRoutes,
     ...authRoutes,
     ...scheduleRoutes,
+
+    GoRoute(
+      path: '/take-attendance',
+      name: 'take_attendance',
+      builder: (context, state) {
+        // Recibimos los argumentos enviados desde el botón
+        final Map<String, dynamic> args = state.extra as Map<String, dynamic>;
+        return TakeAttendancePage(
+          classSessionId: args['sessionId'] as int,
+          courseName: args['courseName'] as String,
+        );
+      },
+    ),
+
+    // --- Aquí empieza el Menú con BottomNavigationBar ---
     StatefulShellRoute.indexedStack(
       builder: (context, state, navigationShell) {
         return BlocProvider(
@@ -41,6 +56,7 @@ final GoRouter appRouter = GoRouter(
       },
 
       branches: [
+        // Rama Perfil
         StatefulShellBranch(
           routes: [
             GoRoute(
@@ -50,6 +66,7 @@ final GoRouter appRouter = GoRouter(
             ),
           ],
         ),
+        // Rama Home
         StatefulShellBranch(
           routes: [
             GoRoute(
@@ -59,27 +76,15 @@ final GoRouter appRouter = GoRouter(
             ),
           ],
         ),
+        // Rama Settings
         StatefulShellBranch(
           routes: [
             GoRoute(
               path: '/settings',
               name: 'settings',
               builder: (context, state) => const SettingsPage(),
-              routes: [
-                GoRoute(
-                  path: 'attendance',
-                  name: 'attendance',
-                  builder: (context, state) {
-                    
-                    final bloc = getIt<AttendanceBloc>()
-                      ..add(const AttendanceInitRequested());
-                    return BlocProvider.value(
-                      value: bloc,
-                      child: const AttendancePage(),
-                    );
-                  },
-                ),
-              ],
+
+              routes: [], 
             ),
           ],
         ),
