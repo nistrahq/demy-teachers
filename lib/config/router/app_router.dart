2fe865fca85e9eff26d0347c6a72ec663ee370d1
Salim Ramirez <salimramirezm@gmail.com>
2025-12-01 16:30:41 -0500

    refactor: remove unused imports from app_router.dart

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,6 +1,3 @@
-import 'package:demy_teachers/core/di/injection.dart';
-import 'package:demy_teachers/features/attendance/presentation/blocs/attendance_bloc.dart';
-import 'package:demy_teachers/features/attendance/presentation/blocs/attendance_event.dart';
 import 'package:demy_teachers/features/attendance/presentation/pages/take_attendance_page.dart'; // Asegúrate de importar esto
 import 'package:demy_teachers/features/auth/presentation/navigation/auth_routes.dart';
 import 'package:demy_teachers/features/auth/presentation/pages/reset_password_page.dart';
