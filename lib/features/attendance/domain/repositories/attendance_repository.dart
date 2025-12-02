92733d0fe6d7841356d8717576de53a3b046516c
diegovilcasaboya <diegovilcatut@gmail.com>
2025-12-02 01:06:30 -0500

    feat(attendance): update attendance repository

--- lib/features/attendance/domain/repositories/attendance_repository.dart
+++ lib/features/attendance/domain/repositories/attendance_repository.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:demy_teachers/core/errors/failure.dart';
+import 'package:demy_teachers/features/attendance/domain/entities/attendance_history.dart';
 import 'package:demy_teachers/features/attendance/domain/entities/student.dart';
 // ... imports entities
 
 abstract class AttendanceRepository {
-  Future<Either<Failure, List<Student>>> getStudents();
+  Future<Either<Failure, List<Student>>> getStudents(int classSessionId);
   Future<Either<Failure, void>> registerAttendance(int sessionId, String date, List<StudentAttendance> attendanceList);
+  Future<Either<Failure, List<AttendanceHistory>>> getAttendanceHistory();
 }
